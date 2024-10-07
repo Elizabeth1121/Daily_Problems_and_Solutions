67a71a9ca06689fe7b0e52df7c87b02e2ebc5947
Elizabeth <itsybitsy1121@outlook.com>
2024-10-07 14:43:38 -0800

    1768. Merge Strings Alternately O(N)

---
+++ LEETCODE_Solutions/Easy/Merge_Strings_Alternetly/Program.cs
@@ -0,0 +1,37 @@
+﻿string word1 = "sakj";
+string word2 = "hijk";
+
+string result = mergedString(word1, word2);
+Console.WriteLine(result);
+
+string mergedString (string word1, string word2)
+{
+    string mergedString = "";
+    int shorterString = 0;
+
+    if(word2.Length < word1.Length)
+    {
+        shorterString = word2.Length;
+    }
+    else
+    {
+        shorterString = word1.Length;
+    }
+
+    for (int i = 0; i < shorterString; i++)
+    {
+        mergedString += word1[i];
+        mergedString += word2[i];
+    }
+
+    if(shorterString == word1.Length)
+    {
+        mergedString += word2.Substring(shorterString);
+    }
+    else
+    {
+        mergedString += word1.Substring(shorterString);
+    }
+
+    return mergedString;
+}
